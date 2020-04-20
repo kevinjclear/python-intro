db8ae0072d38197921aebe587927cd5c1e9120df
Samuel Beguiristain <a@sabe.dev>
2020-04-20 06:30:41 -0400

    Added input for name

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,9 @@
+name = input("What is your name? ")
+
 prompt = "\nTell me something, and I will repeat it back to you: "
 prompt += "\nEnter 'quit' to end program. "
 message = ""
 while message != 'quit':
     message = input(prompt)
     if message != 'quit':
-        print("You said: " + "\"" + message + "\"")
+        print(name + " said: " + "\"" + message + "\"")
